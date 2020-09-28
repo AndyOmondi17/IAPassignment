7706900a65654ecd037b789ec71e0827046950b6
AndyOmondi17 <andyomondi17@gmail.com>
2020-09-28 21:59:49 +0300

    styling forms

---
+++ insert.php
@@ -0,0 +1,10 @@
+<?php
+$Firstname = $_POST['Firstname'];
+$Surname = $_POST['Surname'];
+$Username = $_POST['Username'];
+$Email = $_POST['Email'];
+$City = $_POST['City'];
+$UploadFile = $_POST['UploadFile'];
+$Password = $_POST['Password'];
+
+?>
