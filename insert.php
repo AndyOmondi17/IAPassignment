a90a80e0001931f25e962e37199a48d177cf31f1
AndyOmondi17 <andyomondi17@gmail.com>
2020-09-30 12:38:21 +0300

    implements signing up into an account

--- insert.php
+++ insert.php
@@ -1,24 +1,27 @@
-
 <?php
     $Firstname = $_POST['Firstname'];
     $Surname = $_POST['Surname'];
     $Username = $_POST['Username'];
     $Email = $_POST['Email'];
     $City = $_POST['City'];
-    $UploadFile = $_POST['UploadFile'];
     $Password = $_POST['Password'];
+     $encryptedPassword = md5($Password);
     $ConfirmPassword = $_POST['ConfirmPassword'];
+    $UploadFile = $_FILES['UploadFile']['name'];
+    $temp_profile = $_FILES['UploadFile']['tmp_name'];
+    move_uploaded_File($temp_profile,"images/$UploadFile");
+    
 
     if(!empty($Firstname) || !empty($Surname) || !empty($Username) || !empty($Email) || !empty($UploadFile) || !empty($Password) || !empty($ConfirmPassword)){
-        if(strcmp($ConfirmPassword,$Password) == 0){
+        // if(strcmp($ConfirmPassword,$Password) == 0){}
+         // else{
+        //     echo "Password and Confirm Password does not match";
+        // }
             $user='root';
             $password='';
             $db='userinformation';
 
-            $conn = new mysqli('localhost',$user,$password,$db);
-            if(mysqli_connect_error){
-                die('Connection Error');
-            }else{
+            $conn = new mysqli('localhost',$user,$password,$db) or die("Unable to connect");
 
             $SELECT = "SELECT Email From userdetails Where Email = ? Limit 1";
             $INSERT = "INSERT Into userdetails (Firstname, Surname, Username, Email, City, UploadFile, Password) values (?,?,?,?,?,?,?)";
@@ -33,18 +36,14 @@
             if($rnum==0){
                 $stmt-> close();
                 $stmt = $conn->prepare($INSERT);
-                $stmt->bind_param("s,s,s,s,s,s,s",$Firstname,$Surname,$Username,$Email,$City,$UploadFile,$Password);
+                $stmt->bind_param("sssssss",$Firstname,$Surname,$Username,$Email,$City,$UploadFile,$encryptedPassword);
                 $stmt-> execute();
                 echo "New record inserted succesfully";
             }else{
                 echo "Email already exists";
             }
             $stmt -> close();
-            $conn -> close();
-            }
-        }else{
-            echo "Password and Confirm Password does not match";
-        }
+            $conn -> close();   
     }else{
         echo "All fields are required";
         die();
